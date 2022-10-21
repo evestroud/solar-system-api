ac0d942babaa6aef01b273078e796d7169ecb188
Eve Stroud <evestroud89@gmail.com>
2022-10-21 11:41:55 -0400

    Wave 01 completed

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet:
 
@@ -9,11 +9,27 @@ class Planet:
         self.orbital_period = orbital_period 
 
 
-planets = [Planet(1, "Mercury", "The smallest planet in the solar system", 88),
-Planet(2, "Venus", "Sometimes called earth's sister planet", 225),
-Planet(3, "Earth", "Only astronomical object known to harbor life", 365),
-Planet(4, "Mars", "Named for the Roman god of war", 687),
-Planet(5, "Jupiter", "Its larger than all of the other planetes combined", 4343),
-Planet(6, "Saturn", "Has a prominent ring system", 10759),
-Planet(7, "Uranus", "Named after the greek god of the sky", 30660),
-Planet(8, "Neptune", "Furthest planet from the sun", 60225)  ]
+planets = [
+    Planet(1, "Mercury", "The smallest planet in the solar system", 88),
+    Planet(2, "Venus", "Sometimes called earth's sister planet", 225),
+    Planet(3, "Earth", "Only astronomical object known to harbor life", 365),
+    Planet(4, "Mars", "Named for the Roman god of war", 687),
+    Planet(5, "Jupiter", "Its larger than all of the other planets combined", 4343),
+    Planet(6, "Saturn", "Has a prominent ring system", 10759),
+    Planet(7, "Uranus", "Named after the greek god of the sky", 30660),
+    Planet(8, "Neptune", "Furthest planet from the sun", 60225),  
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def get_planets_info():
+    planets_info = []
+    for planet in planets:
+        planets_info.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "orbital_period": planet.orbital_period,
+        })
+    return jsonify(planets_info)
