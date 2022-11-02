ef98bb6111bedc096adf4abed94eae56c9e4081f
Maggie <mjtice0@gmail.com>
2022-11-01 20:51:14 -0400

    no changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
 
+migrate = Migrate()
+
+
 def create_app(test_config=None):
     app = Flask(__name__)
     
+
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
 
